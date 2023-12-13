778ae7d9c7ad6aa39cd03d51e291793a55d8bd3c
Andrey Rybakov <rybakov.ad@icloud.com>
2023-12-13 02:24:15 +0100

    Add license to the package

--- src/magnopy/__main__.py
+++ src/magnopy/__main__.py
@@ -1,7 +1,25 @@
+# MAGNOPY - Python package for magnons.
+# Copyright (C) 2023-2023 Magnopy Team
+#
+# e-mail: anry@uv.es, web: magnopy.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 from magnopy import __version__
-from magnopy._pinfo import logo, warranty
+from magnopy._pinfo import conditions, logo, warranty
 
 
 def main():
@@ -13,7 +31,7 @@ def main():
         "command",
         default=None,
         help="Which command to run",
-        choices=["logo", "warranty", "version"],
+        choices=["logo", "warranty", "conditions", "version"],
         nargs="?",
     )
     parser.add_argument(
@@ -27,6 +45,8 @@ def main():
         print(logo())
     elif args.command == "warranty":
         print("\n" + warranty() + "\n")
+    elif args.command == "conditions":
+        print("\n" + conditions() + "\n")
     elif args.command == "version" or args.version:
         print(f"Magnopy v{__version__}")
     elif args.command is None:

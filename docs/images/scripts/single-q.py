e41c7ee59b08105f122f6c66f72bb294803fc430
Andrey Rybakov <rybakov.ad@icloud.com>
2023-12-15 11:27:23 +0100

    Remove unsused functions in the docs scripts

--- docs/images/scripts/single-q.py
+++ docs/images/scripts/single-q.py
@@ -1,3 +1,21 @@
+# MAGNOPY - Python package for magnons.
+# Copyright (C) 2023-2023 Magnopy Team
+#
+# e-mail: anry@uv.es, web: magnopy.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from argparse import ArgumentParser
 
@@ -67,78 +85,6 @@ def plot_example(spins, Q, N):
                     plot_arc(fig, arc, color="#63C06F")
 
     return fig
-
-
-def plot_example_3(output_name):
-    fig = go.Figure()
-    plot_vector(fig, (7, 0, 0), label="v", cone_scale=0.1)
-    plot_vector(fig, (0, 0, 2), label="n", cone_scale=0.3)
-    X = np.linspace(0, 7, 8)
-    theta = np.pi / 7
-    for i in range(len(X)):
-        vector = [
-            np.sin(theta) * np.cos(X[i]),
-            np.sin(theta) * np.sin(X[i]),
-            np.cos(theta),
-        ]
-        plot_vector(
-            fig,
-            vector=vector,
-            origin=(X[i], 0, 0),
-            cone_scale=0.4,
-            line_width=4,
-            color="#E7676B",
-        )
-        arc = np.linspace(0, 2 * np.pi, 100)
-        arc = [
-            np.ones_like(arc) * X[i] + np.cos(arc) * np.sin(theta),
-            np.sin(arc) * np.sin(theta),
-            np.ones_like(arc) * np.cos(theta),
-        ]
-        plot_arc(fig, arc=arc, color="#63C06F")
-    zoom = 1
-    save_figure(
-        fig,
-        output_name,
-        camera_keywords=dict(eye=dict(x=2 * zoom, y=-1.4 * zoom, z=1 * zoom)),
-    )
-
-
-def plot_example_4(output_name):
-    fig = go.Figure()
-    plot_vector(fig, (16, 0, 0), label="v", cone_scale=0.1)
-    plot_vector(fig, (0, 0, 4), label="n", cone_scale=0.3)
-    X = np.linspace(0, 7, 8)
-    theta = (np.pi / 7, np.pi / 4)
-    for i in range(len(X)):
-        for a in range(2):
-            vector = [
-                1.5 * np.sin(theta[a]) * np.cos(X[i] * 2 + a),
-                1.5 * np.sin(theta[a]) * np.sin(X[i] * 2 + a),
-                1.5 * np.cos(theta[a]),
-            ]
-            plot_vector(
-                fig,
-                vector=vector,
-                origin=(X[i] * 2 + a, a / 3, 0),
-                cone_scale=0.4,
-                line_width=4,
-                color="#E7676B",
-            )
-            arc = np.linspace(0, 2 * np.pi, 100)
-            arc = [
-                np.ones_like(arc) * (X[i] * 2 + a)
-                + 1.5 * np.cos(arc) * np.sin(theta[a]),
-                1.5 * np.sin(arc) * np.sin(theta[a]) + a / 3,
-                1.5 * np.ones_like(arc) * np.cos(theta[a]),
-            ]
-            plot_arc(fig, arc=arc, color="#63C06F")
-    zoom = 1
-    save_figure(
-        fig,
-        output_name,
-        camera_keywords=dict(eye=dict(x=2 * zoom, y=-1.4 * zoom, z=1 * zoom)),
-    )
 
 
 def main(root_directory):

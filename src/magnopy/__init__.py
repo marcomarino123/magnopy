fdc4349e198bc160f0a7ba40c41a944018330cf0
Andrey Rybakov <rybakov.ad@icloud.com>
2025-02-17 16:14:05 +0100

    Add version to the __init__ file

--- src/magnopy/__init__.py
+++ src/magnopy/__init__.py
@@ -16,7 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = "0.0.0"
+
+__version__ = "0.1.0"
 __doclink__ = "magnopy.org"
 __release_date__ = "undefined"
 __git_commit__ = "undefined"

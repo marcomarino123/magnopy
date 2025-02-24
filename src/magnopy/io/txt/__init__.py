54544effb6fadf5ae86ef54ac27146785947368d
Andrey Rybakov <rybakov.ad@icloud.com>
2025-02-24 19:45:43 +0100

    Run pre-commit on all files

--- src/magnopy/io/txt/__init__.py
+++ src/magnopy/io/txt/__init__.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 from .internal import *
 from .tb2j import *
 from .vampire import *

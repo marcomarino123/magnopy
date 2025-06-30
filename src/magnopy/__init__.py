2a02497a95198b42c423076087ec21586a858662
Andrey Rybakov <rybakov.ad@icloud.com>
2025-06-30 19:41:59 +0200

    Prepare release

--- src/magnopy/__init__.py
+++ src/magnopy/__init__.py
@@ -17,9 +17,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __doclink__ = "magnopy.org"
-__release_date__ = "26 June 2025"
+__release_date__ = "30 June 2025"
 
 from . import _constants, examples, io, scenarios
 from ._diagonalization import *
